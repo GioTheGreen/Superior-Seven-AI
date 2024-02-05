8e4f939ed259efd68cb21574ed944a84ab3c3592
O1lec1 <55832256+O1lec1@users.noreply.github.com>
2024-02-05 16:48:15 +0000

    Rickardoless push

--- TwoAndAHalfGods/Assets/BOB/Gen1.cs
+++ TwoAndAHalfGods/Assets/BOB/Gen1.cs
@@ -1,35 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+
 
 public class Gen1 : MonoBehaviour
 {
-    public 
-    float x = 0;
-    bool first = true;
+    public GameObject x;
+    
+
+
+    List<GameObject> landpoints = new List<GameObject>();
+  GameObject hold = null;
+    private GameObject a;
+
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        if (first)
-        {
-            transform.position = new Vector3(-300, 0, 0);
-            print(transform.position);
-            Instantiate(this);
-            transform.position = new Vector3(-200, 0, 0);
-            Instantiate(this);
-            transform.position = new Vector3(-100, 0, 0);
-            Instantiate(this);
-            transform.position = new Vector3(-000, 0, 0);
-            Instantiate(this);
-            first = false;
+        float y = 0;
+        int i= 0;
+        while (i < 3600)
+            {
+            
+                hold = (Instantiate(x, new Vector3((i / 60) * 10f, 0 , (i % 60) * 10f),Quaternion.identity)) as GameObject;
+                landpoints.Add(hold);
+                print(hold.transform.position);
+                i++;
         }
+
+
+
+
+            //transform.position = new Vector3(-300, 0, 0);
+            
+            //Instantiate(X, this.transform);
+            //transform.position = new Vector3(-200, 0, 0);
+            //Instantiate(X, this.transform);
+            //transform.position = new Vector3(-100, 0, 0);
+            //Instantiate(X, this.transform);
+            //transform.position = new Vector3(-000, 0, 0);
+            //Instantiate(X,this.transform);
+          
     }
 
     // Update is called once per frame
     void Update()
     {
-        x = x + 0.1f;
-        
-        
+  
+
+
     }
 }
